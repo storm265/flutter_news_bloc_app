39af9b809fe1954665b065a7190ac74d9cfe8f39
peter3374 <peter4533@mail.ru>
2023-05-25 14:32:12 +0300

    fixed ui

--- lib/presentation/detailed_news_page/detailed_news_page.dart
+++ lib/presentation/detailed_news_page/detailed_news_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
@@ -15,6 +17,7 @@ class DetailedNewsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    log('usrl ${articleModel.urlToImage}');
     return CupertinoPageScaffold(
       child: Column(
         children: [
@@ -22,64 +25,46 @@ class DetailedNewsPage extends StatelessWidget {
             alignment: Alignment.bottomCenter,
             children: [
               Image.network(
-                articleModel.urlToImage!,
+                articleModel.urlToImage ?? '',
                 width: double.infinity,
-                color: articleModel.urlToImage == null
-                    ? CupertinoColors.white
-                    : null,
+                errorBuilder: (context, error, stackTrace) => const SizedBox(),
               ),
-              BlurWidget(
-                width: double.infinity,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 5),
-                  child: Text(
-                    articleModel.title!,
-                    textAlign: TextAlign.center,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      color: CupertinoColors.white,
-                      fontSize: 20,
+              Align(
+                alignment: Alignment.bottomRight,
+                child: BlurWidget(
+                  width: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      articleModel.title ?? '',
+                      textAlign: TextAlign.center,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        color: CupertinoColors.white,
+                        fontSize: 20,
+                      ),
                     ),
                   ),
                 ),
               ),
               Positioned(
-                right: 350,
-                bottom: 165,
-                child: GestureDetector(
-                  onTap: () => AutoRouter.of(context).pop(),
-                  child: const BlurWidget(
-                    child: Icon(
-                      CupertinoIcons.back,
-                      size: 35,
+                top: 15,
+                left: 15,
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: GestureDetector(
+                    onTap: () async => await AutoRouter.of(context).pop(),
+                    child: const BlurWidget(
+                      child: Icon(
+                        CupertinoIcons.back,
+                        size: 35,
+                      ),
                     ),
                   ),
                 ),
               ),
             ],
-          ),
-          articleModel.author == null
-              ? const SizedBox()
-              : Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'author: ${articleModel.author!}',
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w200,
-                    ),
-                  ),
-                ),
-          Text(
-            articleModel.publishedAt != null
-                ? DateFormat('dd.MM')
-                    .add_jm()
-                    .format(DateTime.parse(articleModel.publishedAt!))
-                : '',
-            style: const TextStyle(
-              fontWeight: FontWeight.w300,
-              fontSize: 14,
-            ),
           ),
           Container(
             padding: const EdgeInsets.all(12),
@@ -95,6 +80,28 @@ class DetailedNewsPage extends StatelessWidget {
             ),
             child: Text(articleModel.content!),
           ),
+          Text(
+            articleModel.publishedAt != null
+                ? DateFormat('dd.MM')
+                    .add_jm()
+                    .format(DateTime.parse(articleModel.publishedAt!))
+                : '',
+            style: const TextStyle(
+              fontWeight: FontWeight.w300,
+              fontSize: 14,
+            ),
+          ),
+          articleModel.author == null
+              ? const SizedBox()
+              : Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Written by: ${articleModel.author!}',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w200,
+                    ),
+                  ),
+                ),
         ],
       ),
     );
